1c93f3cca27a2c3b6205e49a506fc76a7ca8f990
Darryl Kilzer <darrylkilzer@gmail.com>
2020-02-06 21:25:14 -0700

    keeps, vaultkeeps, vaults basic tests

--- src/tests/Keeps.test.js
+++ src/tests/Keeps.test.js
@@ -20,194 +20,248 @@ export class KeepsSuite extends UtilitySuite {
     super("Keeps Testing", PATH);
     SetAuth(this.request);
     this.addTests(
-      new Test(
-        {
-          name: "Can Create a keep",
-          path: PATH,
-          description:
-            "POST request. This should create a new keep in your database. UserId is attached on the server side",
-          expected: "Keep",
-          payload: "Keep object {name, description, img, isPrivate}"
-        },
-        async () => {
-          let keep;
-          try {
-            let user = await this.CheckUserAsync();
-            keep = await this.create({
-              ...keepObj,
-              userId: "dont trust the front end"
-            });
-            this.verifyIsSame(keepObj, keep);
-            if (
-              keep.userId == "dont trust the front end" ||
-              keep.userId != user.sub
-            ) {
-              return this.fail(
-                "The keep.userId differs from the logged in users id. Users can create a keep with any user id."
-              );
-            }
-            return this.pass("Successfully created a keep!", keep);
-          } catch (e) {
-            return this.unexpected(keepObj, this.handleError(e));
-          } finally {
-            if (keep) {
-              await this.delete(keep.id);
-            }
-          }
-        }
-      ),
-      new Test(
-        {
-          name: "Can Get Public Keeps",
-          path: PATH,
-          description: "GET request. This should get a list of public keeps.",
-          expected: "Keep[]"
-        },
-        async () => {
-          let keeps;
-          try {
-            let user = await this.CheckUserAsync();
-            //FIXME this wont work anymore
-            // let keep = await this.create(keepObj);
-            // this.switchUserAsync();
-            keeps = await this.get();
-            if (keeps.length == 0) {
-              return this.fail(
-                "Please add at least one keep to test this route."
-              );
-            } else if (!keeps.every(k => !k.isPrivate || k.userId == user.id)) {
-              return this.fail(
-                "Able to retrieve private keeps that do not belong to the user."
-              );
-            } else if (!this.verifyIsSame(keepObj, keeps[0])) {
-              return this.fail(
-                "Array does not contain objects that match the given Keep model"
-              );
-            }
-            return this.pass("Able to get keeps", keeps.splice(0, 3));
-          } catch (e) {
-            return this.unexpected([keepObj], this.handleError(e));
-          }
-        }
-      ),
-      new Test(
-        {
-          name: "Can Get keep by Id",
-          path: PATH + "/:id",
-          description: "GET request. This should get one keep by its id.",
-          expected: "Keep"
-        },
-        async () => {
-          let keeps;
-          let keep;
-          try {
-            keeps = await this.get();
-            if (keeps.length == 0) {
-              return this.fail(
-                "Please add at least one keep to test this route."
-              );
-            }
-            keep = await this.getById(keeps[0].id);
-            if (keeps[0].id != keep.id) {
-              return this.fail("Could not retrieve the keep by its Id.");
-            }
-            return this.pass("Retrieved Keep by Id", keep);
-          } catch (e) {
-            return this.unexpected(keepObj, this.handleError(e));
-          }
-        }
-      ),
-      new Test(
-        {
-          name: "Can Edit keep by Id",
-          path: PATH + "/:id",
-          description: "PUT request. This should update one keep by its id.",
-          expected: "Keep",
-          payload: "Keep"
-        },
-        async () => {
-          let keep;
-          let updatedKeep;
-          try {
-            let newKeep = { ...keepObj };
-            await this.CheckUserAsync();
-            keep = await this.create(newKeep);
-            let editedKeep = { ...keep };
-            editedKeep.name = "edited keep";
-            await this.update(editedKeep);
-            updatedKeep = await this.getById(editedKeep.id);
-            if (updatedKeep.name != editedKeep.name) {
-              return this.fail("Could not edit the keep.");
-            }
-            return this.pass("Successfully edited the keep!", updatedKeep);
-          } catch (e) {
-            return this.unexpected(keepObj, this.handleError(e));
-          } finally {
-            if (keep) {
-              await this.delete(keep.id);
-            }
-          }
-        }
-      ),
-      new Test(
-        {
-          name: "Can delete keep by Id",
-          path: PATH + "/:id",
-          description: "DELETE request. This should delete one keep by its id.",
-          expected: "Keep"
-        },
-        async () => {
-          let keep;
-          try {
-            keep = await this.create({
-              name: "TEST__KEEP__DELETABLE",
-              description: "KEEP__DESCRIPTION_SHOULD_GET_DELETED",
-              img: "//placehold.it/200x200",
-              isPrivate: false
-            });
-          } catch (e) {
-            return this.unexpected(keep, e.response.data);
-          }
-
-          try {
-            await this.delete(keep.id);
-          } catch (e) {
-            return this.fail(e.message);
-          }
-
-          try {
-            await this.getById(keep.id);
-            return this.fail("Unable to delete keep by its Id");
-          } catch (e) {
-            return this.pass("Sucessfully removed keep by it's Id", keep);
-          }
-        }
-      ),
-      new Test(
-        {
-          name: "Can create Private keeps",
-          path: PATH,
-          description: "Create private keeps",
-          expected: "Keep"
-        },
-        async () => {
-          let keep;
-          try {
-            keep = await this.create({
-              name: "TEST__KEEP__PRIVATE",
-              description: "KEEP__SHOULD_BE_PRIVATE",
-              img: "//placehold.it/200x200",
-              isPrivate: true
-            });
-            if (!keep.isPrivate) {
-              return this.fail("Could not make a private keep");
-            }
-            return this.pass("Able to create private keeps", keep);
-          } catch (e) {
-            return this.unexpected(keep, this.handleError(e));
-          }
-        }
-      )
+      this.canCreatePublicKeep(),
+      this.canCreatePrivateKeep(),
+      this.canGetPublicKeeps(),
+      this.canGetKeepById(),
+      this.canEditKeepById(),
+      // REVIEW do we want this?
+      // this.cantEditViewsKeepsSharesUserId(),
+      this.canDeleteKeepById()
     );
+  }
+
+  canCreatePublicKeep() {
+    return new Test(
+      {
+        name: "Can Create a Public Keep",
+        path: PATH,
+        description:
+          "POST request. This should create a new public keep in your database.",
+        expected: "Keep",
+        payload: "Keep object {name, description, img, isPrivate}"
+      },
+      async () => {
+        let keep;
+        try {
+          keep = await this.create({
+            ...keepObj,
+            userId: "dont trust the front end"
+          });
+          this.verifyIsSame(keepObj, keep);
+          return this.pass("Successfully created a keep!", keep);
+        } catch (e) {
+          return this.unexpected(keepObj, this.handleError(e));
+        } finally {
+          if (keep) {
+            await this.delete(keep.id);
+          }
+        }
+      }
+    )
+  }
+
+  canCreatePrivateKeep() {
+    return new Test(
+      {
+        name: "Can create Private keeps",
+        path: PATH,
+        description: "POST request. This should create a new public keep in your database.",
+        expected: "Keep"
+      },
+      async () => {
+        let keep;
+        try {
+          keep = await this.create({
+            name: "TEST__KEEP__PRIVATE",
+            description: "KEEP__SHOULD_BE_PRIVATE",
+            img: "//placehold.it/200x200",
+            isPrivate: true
+          });
+          if (!keep.isPrivate) {
+            return this.fail("Could not make a private keep")
+          }
+          return this.pass("Able to create a private keep", keep);
+        } catch (e) {
+          return this.unexpected(keep, this.handleError(e));
+        }
+      }
+    )
+  }
+
+
+  canGetPublicKeeps() {
+    return new Test(
+      {
+        name: "Can Get Public Keeps",
+        path: PATH,
+        description: "GET request. This should get a list of public keeps.",
+        expected: "Keep[]"
+      },
+      async () => {
+        let keeps;
+        try {
+          let user = await this.CheckUserAsync();
+          keeps = await this.get();
+          if (keeps.length == 0) {
+            return this.fail(
+              "Please add at least one keep to test this route."
+            );
+          } else if (!keeps.every(k => !k.isPrivate || k.userId == user.id)) {
+            return this.fail(
+              "Able to retrieve private keeps that do not belong to the user."
+            );
+          } else if (!this.verifyIsSame(keepObj, keeps[0])) {
+            return this.fail(
+              "Array does not contain objects that match the given Keep model."
+            );
+          }
+          return this.pass("Able to get keeps", keeps.splice(0, 3));
+        } catch (e) {
+          return this.unexpected([keepObj], this.handleError(e));
+        }
+      }
+    )
+  }
+
+  canGetKeepById() {
+    return new Test(
+      {
+        name: "Can Get keep by Id",
+        path: PATH + "/:id",
+        description: "GET request. This should get one keep by its id.",
+        expected: "Keep"
+      },
+      async () => {
+        try {
+          let keeps = await this.get();
+          if (keeps.length < 1) {
+            return this.fail(
+              "Please add at least one keep to test this route."
+            );
+          }
+          let keep = await this.getById(keeps[0].id);
+          if (keeps[0].id != keep.id) {
+            return this.fail("Could not retrieve the keep by its Id.");
+          }
+          return this.pass("Retrieved Keep by Id", keep);
+        } catch (e) {
+          return this.unexpected(keepObj, this.handleError(e));
+        }
+      }
+    )
+  }
+
+  canEditKeepById() {
+    return new Test(
+      {
+        name: "Can Edit keep by Id",
+        path: PATH + "/:id",
+        description: "PUT request. This should update one keep by its id.",
+        expected: "Keep",
+        payload: "Keep"
+      },
+      async () => {
+        let keep;
+        let updatedKeep;
+        try {
+          await this.CheckUserAsync();
+          let newKeep = { ...keepObj };
+          keep = await this.create(newKeep);
+          let editedKeep = { ...keep };
+          editedKeep.name = "edited keep";
+          await this.update(editedKeep);
+          updatedKeep = await this.getById(editedKeep.id);
+          if (updatedKeep.name != editedKeep.name) {
+            return this.fail("Could not edit the keep.");
+          }
+          return this.pass("Successfully edited the keep!", updatedKeep);
+        } catch (e) {
+          return this.unexpected(keepObj, this.handleError(e));
+        } finally {
+          if (keep) {
+            await this.delete(keep.id);
+          }
+        }
+      }
+    )
+  }
+
+  canDeleteKeepById() {
+    return new Test(
+      {
+        name: "Can delete keep by Id",
+        path: PATH + "/:id",
+        description: "DELETE request. This should delete one keep by its id.",
+        expected: "Keep"
+      },
+      async () => {
+        let keep;
+        try {
+          keep = await this.create({
+            name: "TEST__KEEP__DELETABLE",
+            description: "KEEP__DESCRIPTION_SHOULD_GET_DELETED",
+            img: "//placehold.it/200x200",
+            isPrivate: false
+          });
+        } catch (e) {
+          return this.unexpected(keep, e.response.data);
+        }
+
+        try {
+          await this.delete(keep.id);
+        } catch (e) {
+          return this.fail(e.message);
+        }
+
+        try {
+          await this.getById(keep.id);
+          return this.fail("Unable to delete keep by its Id");
+        } catch (e) {
+          return this.pass("Sucessfully removed keep by it's Id", keep);
+        }
+      }
+    )
+  }
+
+  cantEditViewsKeepsSharesUserId() {
+    return new Test({
+      name: "Cant edit shares, views, keeps, or userId",
+      path: PATH + "/:id",
+      description: "PUT request. Cant edit shares, views, keeps, or userId from the front end on a keep.",
+      expected: "Keep",
+      payload: "Keep"
+    },
+      async () => {
+        let keep;
+        let updatedKeep;
+        try {
+          let newKeep = { ...keepObj };
+          keep = await this.create(newKeep);
+          let editedKeep = { ...keep };
+
+          editedKeep.shares = 10;
+          editedKeep.views = 10;
+          editedKeep.keeps = 10;
+          editedKeep.userId = "Oops. You allowed the front end to change your userId"
+
+          await this.update(editedKeep);
+          updatedKeep = await this.getById(editedKeep.id);
+          console.log(updatedKeep)
+          for (let key in updatedKeep) {
+            if (updatedKeep[key] != keep[key]) {
+              return this.fail(`Was able to edit the field ${key} on the keep.`);
+            }
+          }
+          return this.pass("Couldn't edit the shares, views, keeps, or userId.", updatedKeep);
+        } catch (e) {
+          return this.unexpected(keepObj, this.handleError(e));
+        } finally {
+          if (keep) {
+            await this.delete(keep.id);
+          }
+        }
+      }
+    )
   }
 }
--- src/tests/Vaults.test.js
+++ src/tests/Vaults.test.js
@@ -12,132 +12,87 @@ export class VaultsSuite extends UtilitySuite {
   constructor() {
     super("Vaults Testing", PATH)
     this.addTests(
-      new Test({
-        name: 'Can Create a vault',
-        path: PATH,
-        description: 'POST request. This should create a new vault in your database. UserId is attached on the server side',
-        expected: 'Vault',
-        payload: 'Vault object { name, description }'
-      },
-        async () => {
-          let vault
-          try {
-            let user = await this.CheckUserAsync()
-            vault = await this.create({ ...vaultObj, userId: "dont trust the front end" })
-            this.verifyIsSame(vaultObj, vault)
-            if (vault.userId != user.id) {
-              return this.fail("Users can create a vault with any user id.")
-            }
-            return this.pass("Successfully created a vault!", vault)
-          } catch (e) {
-            return this.unexpected(vaultObj, this.handleError(e))
-          } finally {
-            if (vault) {
-              let vaults = await this.get()
-              if (vaults.length > 1) {
-                // @ts-ignore
-                await this.delete(vault.id)
-              }
-            }
-          }
-        }
-      ),
-      new Test({
-        name: 'Can get a vault by its id',
-        path: PATH,
-        description: 'GET request. This should get a single vault by its id, as long as the user owns it.',
-        expected: 'Vault',
-        payload: `Vault { }`
-      },
-        async () => {
-          try {
-            let user = await this.CheckUserAsync()
-            let userOne = JSON.parse(localStorage.getItem('user'))
-            let userTwo
-            let vaults = await this.getVaultsAsync()
-            vaults = vaults.filter(v => v.userId == user.id)
-            if (vaults.length == 0) {
-              return this.fail('Please add at least one vault with this user to test this route.')
-            }
-            await this.delete('logout', 'https://localhost:5001/account')
-            let users = JSON.parse(localStorage.getItem('users'))
-            let index = Math.floor(Math.random() * users.length)
-            userTwo = users[index]
-            userTwo["username"] = `${userTwo.name} ${userTwo.surname}`
-            users.splice(index, 1)
-            localStorage.setItem('users', JSON.stringify(users))
-            await this.create(userTwo, 'https://localhost:5001/account/register')
-            try {
-              let vault = await this.getById(vaults[0].id)
-              return this.fail("Able to retrieve a vault that doesnt belong to the user.")
-            } catch (e) { }
-            await this.create(userOne, 'https://localhost:5001/account/login')
-            let vault = await this.getById(vaults[0].id)
-            return this.pass("Able to get a users vault by its id.", vault)
-          } catch (e) {
-            return this.unexpected([vaultObj], this.handleError(e))
-          }
-        }
-      ),
-      new Test({
-        name: 'Can get users vaults',
-        path: PATH,
-        description: 'GET request. This should get all the users vaults',
-        expected: 'Vault [ ]',
-        payload: ''
-      },
-        async () => {
-          try {
-            let user = await this.CheckUserAsync()
-            let vaults = await this.get()
-            if (vaults.length == 0) {
-              return this.fail('Please add at least one vault to test this route.')
-            }
-            else if (!vaults.every(v => v.userId == user.id)) {
-              return this.fail("Able to retrieve vaults that do not belong to the user.")
-            }
-            else if (!this.verifyIsSame(vaultObj, vaults[0])) {
-              return this.fail("Array does not contain objects that match the given Vault model")
-            }
-            return this.pass("Able to get users vaults", vaults.splice(0, 3))
-          } catch (e) {
-            return this.unexpected([vaultObj], this.handleError(e))
-          }
-        }
-      ),
-      new Test({
-        name: 'Can delete a vault',
-        path: PATH,
-        description: 'DELETE request. This should delete a vault if the user owns it.',
-        expected: '',
-        payload: ''
-      },
-        async () => {
-          try {
-            await this.CheckUserAsync()
-            let userOne = JSON.parse(localStorage.getItem('user'))
-            let userTwo
-            let vault = await this.create(vaultObj)
-            await this.delete('logout', 'https://localhost:5001/account')
-            let users = JSON.parse(localStorage.getItem('users'))
-            let index = Math.floor(Math.random() * users.length)
-            userTwo = users[index]
-            userTwo["username"] = `${userTwo.name} ${userTwo.surname}`
-            users.splice(index, 1)
-            localStorage.setItem('users', JSON.stringify(users))
-            await this.create(userTwo, 'https://localhost:5001/account/register')
-            try {
-              await this.delete(vault.id)
-              return this.fail("User able to delete a vault they don't own.")
-            } catch (e) {
-              await this.create(userOne, 'https://localhost:5001/account/login')
-              await this.delete(vault.id)
-              return this.pass("Able to delete vaults!")
-            }
-          } catch (e) {
-            this.unexpected("Vault deleted", this.handleError(e))
-          }
-        })
+      this.canCreateVault(),
+      this.canGetVaultById(),
+      this.canDeleteVault()
     )
   }
+
+  canCreateVault() {
+    return new Test({
+      name: 'Can Create a vault',
+      path: PATH,
+      description: 'POST request. This should create a new vault.',
+      expected: 'Vault',
+      payload: 'Vault object { name, description }'
+    },
+      async () => {
+        let vault
+        try {
+          vault = await this.create({ ...vaultObj, userId: "dont trust the front end" })
+          this.verifyIsSame(vaultObj, vault)
+          return this.pass("Successfully created a vault!", vault)
+        } catch (e) {
+          return this.unexpected(vaultObj, this.handleError(e))
+        } finally {
+          if (vault) {
+            let vaults = await this.get()
+            if (vaults.length > 1) {
+              // @ts-ignore
+              await this.delete(vault.id)
+            }
+          }
+        }
+      }
+    )
+  }
+
+  canGetVaultById() {
+    return new Test({
+      name: 'Can get a vault by its id',
+      path: PATH,
+      description: 'GET request. This should get a single vault by its id.',
+      expected: 'Vault',
+      payload: `Vault { }`
+    },
+      async () => {
+        try {
+          let vaults = await this.get("https://localhost:5001/api/vaults");
+          if (vaults.length < 1) {
+            return this.fail('Please create at least one vault to test this route.')
+          }
+          let vault = await this.getById(vaults[0].id)
+          return this.pass("Able to get a users vault by its id.", vault)
+        } catch (e) {
+          return this.unexpected([vaultObj], this.handleError(e))
+        }
+      }
+    )
+  }
+
+
+  canDeleteVault() {
+    return new Test({
+      name: 'Can delete a vault',
+      path: PATH,
+      description: 'DELETE request. This should delete a vault.',
+      expected: '',
+      payload: ''
+    },
+      async () => {
+        try {
+          let vault = await this.create(vaultObj)
+          await this.delete(vault.id)
+        } catch (e) {
+          return this.unexpected("Vault deleted", this.handleError(e))
+        }
+        try {
+          await this.getById(vault.id)
+          return this.fail("Vault was not deleted from the database.")
+        } catch (e) {
+          return this.pass("Vault was deleted!")
+        }
+      })
+  }
+
 }
